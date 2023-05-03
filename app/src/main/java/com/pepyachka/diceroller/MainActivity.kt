fa3079902b87ac63afd6a49e723f5faad3b9d1e8
Nikita Drozdov <ndrozdov9@gmail.com>
2023-05-03 23:02:33 +0300

    first commit

---
+++ app/src/main/java/com/pepyachka/diceroller/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.pepyachka.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.pepyachka.diceroller.databinding.ActivityMainBinding
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.rollButton.setOnClickListener {
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        val randomNumber = Random.nextInt(1, 7)
+        val drawableResource = when (randomNumber) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        binding.diceImage.setImageResource(drawableResource)
+        if (randomNumber == 6) {
+            Toast.makeText(this, "You win!", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
